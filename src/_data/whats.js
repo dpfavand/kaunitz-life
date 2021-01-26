e14fb51ec72de1d39aff5654df9a204521b30178
Daniel Favand <daniel@favand.net>
2021-01-25 21:32:52 -0500

    data and styles update

--- src/_data/whats.js
+++ src/_data/whats.js
@@ -3,9 +3,10 @@ const client = require('../../sanityClient');
 const query = `
   *[_type == 'topic' && kind == 'does'] {
     title,
-    shortDescription,
+    introduction,
     'slug': slug.current,
-    'logo': logo.asset ->
+    'logo': logo.asset ->,
+    promo 
   }
 `;
 
